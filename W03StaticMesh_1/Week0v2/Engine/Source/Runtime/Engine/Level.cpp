6adcb16668d589067f723459e06b2675aacf6c85
choux-croquant <choifrance@gmail.com>
2025-04-02 11:41:13 +0900

    update level base

--- W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Engine/Level.cpp
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Engine/Level.cpp
@@ -1,1 +1,119 @@
+#pragma once
+
 #include "Level.h"
+
+#include "Actors/Player.h"
+#include "BaseGizmos/TransformGizmo.h"
+#include "Camera/CameraComponent.h"
+#include "LevelEditor/SLevelEditor.h"
+#include "Engine/FLoaderOBJ.h"
+#include "Engine/StaticMeshActor.h"
+#include "Components/SkySphereComponent.h"
+#include "Classes/Components/StaticMeshComponent.h"
+
+#include "Runtime/Core/Math/Octree.h"
+#include "Runtime/Core/Math/OctreeNode.h"
+
+void ULevel::Initialize()
+{
+    // TODO: Load Scene
+    CreateBaseObject();
+    //SpawnObject(OBJ_CUBE);
+    //FManagerOBJ::CreateStaticMesh("Assets/Dodge/Dodge.obj");
+
+    //FManagerOBJ::CreateStaticMesh("Assets/SkySphere.obj");
+    //AActor* SpawnedActor = SpawnActor<AActor>();
+    //USkySphereComponent* skySphere = SpawnedActor->AddComponent<USkySphereComponent>();
+    //skySphere->SetStaticMesh(FManagerOBJ::GetStaticMesh(L"SkySphere.obj"));
+    //skySphere->GetStaticMesh()->GetMaterials()[0]->Material->SetDiffuse(FVector((float)32/255, (float)171/255, (float)191/255));
+}
+
+void ULevel::CreateBaseObject()
+{
+    
+}
+
+void ULevel::ReleaseBaseObject()
+{
+
+}
+
+void ULevel::Tick(float DeltaTime)
+{
+    for (AActor* Actor : PendingBeginPlayActors)
+    {
+        Actor->BeginPlay();
+    }
+    PendingBeginPlayActors.Empty();
+
+    for (AActor* Actor : ActorsArray)
+    {
+        Actor->Tick(DeltaTime);
+    }
+}
+
+void ULevel::Release()
+{
+    for (AActor* Actor : ActorsArray)
+    {
+        Actor->EndPlay(EEndPlayReason::WorldTransition);
+        TSet<UActorComponent*> Components = Actor->GetComponents();
+        for (UActorComponent* Component : Components)
+        {
+            GUObjectArray.MarkRemoveObject(Component);
+        }
+        GUObjectArray.MarkRemoveObject(Actor);
+    }
+    ActorsArray.Empty();
+
+    ReleaseBaseObject();
+
+    GUObjectArray.ProcessPendingDestroyObjects();
+}
+
+bool ULevel::DestroyActor(AActor* ThisActor)
+{
+    if (ThisActor->GetWorld() == nullptr)
+    {
+        return false;
+    }
+
+    if (ThisActor->IsActorBeingDestroyed())
+    {
+        return true;
+    }
+
+    // 액터의 Destroyed 호출
+    ThisActor->Destroyed();
+
+    if (ThisActor->GetOwner())
+    {
+        ThisActor->SetOwner(nullptr);
+    }
+
+    TSet<UActorComponent*> Components = ThisActor->GetComponents();
+    for (UActorComponent* Component : Components)
+    {
+        Component->DestroyComponent();
+    }
+
+    // World에서 제거
+    ActorsArray.Remove(ThisActor);
+
+    // 제거 대기열에 추가
+    GUObjectArray.MarkRemoveObject(ThisActor);
+    return true;
+}
+
+template <typename T>
+    requires std::derived_from<T, AActor>
+T* ULevel::SpawnActor()
+{
+    T* Actor = FObjectFactory::ConstructObject<T>();
+    // TODO: 일단 AddComponent에서 Component마다 초기화
+    // 추후에 RegisterComponent() 만들어지면 주석 해제
+    // Actor->InitializeComponents();
+    ActorsArray.Add(Actor);
+    PendingBeginPlayActors.Add(Actor);
+    return Actor;
+}
--- W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Engine/Level.h
+++ W03StaticMesh_1/Week0v2/Engine/Source/Runtime/Engine/Level.h
@@ -3,12 +3,41 @@
 #include "Runtime/CoreUObject/UObject/Object.h"
 #include "Runtime/Engine/Classes/GameFramework/Actor.h"
 
+class Octree;
+
 // 헤더만 만들어 둠, 현재 기능 업음
 class ULevel : public UObject
 {
-public:
-    const TArray<AActor*>& GetActors() const { return Actors; }
-private:
-    TArray<AActor*> Actors;
-};
+    DECLARE_CLASS(ULevel, UObject)
 
+public:
+    ULevel() = default;
+
+    FString LevelName;
+    UWorld* OwningWorld;
+
+    void Initialize();
+    void Tick(float DeltaTime);
+    void Release();
+
+    // 액터 관리 함수들
+    template <typename T>
+        requires std::derived_from<T, AActor>
+    T* SpawnActor();
+
+    bool DestroyActor(AActor* ThisActor);
+
+    const TSet<AActor*>& GetActors() const { return ActorsArray; }
+
+    void CreateBaseObject();
+    void ReleaseBaseObject();
+
+private:
+    // 액터 관리 변수들
+    TSet<AActor*> ActorsArray;
+    TArray<AActor*> PendingBeginPlayActors;
+
+    // 옥트리 관련 변수들
+    Octree* LevelOctree = nullptr;
+    TArray<UObject*> OctreeObjects;
+};
